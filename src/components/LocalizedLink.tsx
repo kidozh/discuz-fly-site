82054d5e9275045b9cb97441ef097f424abe5dc6
kidozh <11661760+kidozh@users.noreply.github.com>
2025-09-20 23:32:36 +0800

    Updates

--- src/components/LocalizedLink.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'gatsby-plugin-react-i18next'
-
-const LocalizedLink: React.FC<any> = (props) => {
-  // Simple wrapper to centralize future link logic and keep usage ergonomic
-  return <Link {...props} />
-}
-
-export default LocalizedLink
--- src/components/Seo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-// Legacy Seo component replaced by Head API usage. Keep a no-op
-// component here to avoid lingering import errors in builds/tools.
-const Seo: React.FC = () => null
-
-export default Seo
